e5fc14cebc099439ccb4af16643e9adbd0e37652
Nekena Andriniaina <andriniainanekena@gmail.com>
2025-08-06 23:19:14 +0300

    fix(employee): secure salary field in FullTimeEmployee with validation

--- src/employee_management/FullTimeEmployee.java
+++ src/employee_management/FullTimeEmployee.java
@@ -1,15 +1,35 @@
 package employee_management;
 
 public class FullTimeEmployee {
-    public int employeeId;
-    public String name;
-    public double monthlySalary;
+    private int employeeId;
+    private String name;
+    private double monthlySalary;
 
     public FullTimeEmployee(int employeeId, String name, double monthlySalary) {
         this.employeeId = employeeId;
         this.name = name;
+        setMonthlySalary(monthlySalary);
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(double monthlySalary) {
+    if (monthlySalary < 0) {
+        this.monthlySalary = 0;
+    } else {
         this.monthlySalary = monthlySalary;
     }
+}
 
     public double calculatePay() {
         return monthlySalary;
