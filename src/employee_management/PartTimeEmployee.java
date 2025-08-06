2b44c9603d6064565767d1237ebfaf03fd840053
Nekena Andriniaina <andriniainanekena@gmail.com>
2025-08-06 23:38:11 +0300

    fix: encapsulate fields and add validation for hoursWorked and hourlyRate in constructor

--- src/employee_management/PartTimeEmployee.java
+++ src/employee_management/PartTimeEmployee.java
@@ -1,31 +1,24 @@
 package employee_management;
 
-public class PartTimeEmployee {
+public class PartTimeEmployee implements Employee {
     private int employeeId;
     private String name;
     private double hoursWorked;
     private double hourlyRate;
 
-
     public PartTimeEmployee(int employeeId, String name, double hoursWorked, double hourlyRate) {
         this.employeeId = employeeId;
         this.name = name;
-        if (hoursWorked < 0) {
-            this.hoursWorked = 0;
-        } else {
-            this.hoursWorked = hoursWorked;
-        }
-        if(hourlyRate < 0) {
-            this.hourlyRate = 0;
-        } else {
-            this.hourlyRate = hourlyRate;
-        }
+        setHoursWorked(hoursWorked);
+        setHourlyRate(hourlyRate);
     }
 
+    @Override
     public int getEmployeeId() {
         return employeeId;
     }
 
+    @Override
     public String getName() {
         return name;
     }
@@ -34,12 +27,28 @@ public class PartTimeEmployee {
         return hoursWorked;
     }
 
+    public void setHoursWorked(double hoursWorked) {
+        if (hoursWorked < 0) {
+            this.hoursWorked = 0;
+        } else {
+            this.hoursWorked = hoursWorked;
+        }
+    }
+
     public double getHourlyRate() {
         return hourlyRate;
     }
 
+    public void setHourlyRate(double hourlyRate) {
+        if (hourlyRate < 0) {
+            this.hourlyRate = 0;
+        } else {
+            this.hourlyRate = hourlyRate;
+        }
+    }
+
+    @Override
     public double calculatePay() {
         return hoursWorked * hourlyRate;
     }
 }
-
